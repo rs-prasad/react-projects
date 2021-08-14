2caf05f99cd1bd9a95c8efe25a4f82af787f2c28
Rabi Prasad <rabiprasad607@gmail.com>
2021-08-14 21:36:16 +0530

    starter project downloaded and birthday-remider project done

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <section className="container">
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
