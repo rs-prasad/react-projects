ac37fec10a5718395fe6ca2fa23b3d8b7dd85278
Rabi Prasad <rabiprasad607@gmail.com>
2021-09-23 21:58:00 +0530

    completed 05-menu

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,39 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useEffect, useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => getCategories(items), []);
+
+  const getCategories = (data) => {
+    let categoriesList = data.map((item) => item.category);
+    categoriesList = [...new Set(categoriesList)];
+    categoriesList.unshift("All");
+    setCategories(categoriesList);
+  };
+  const filterItems = (category) => {
+    let newItems = items;
+    if (category !== "All") {
+      newItems = items.filter((item) => item.category === category);
+    }
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
