10d178581cbbea3dd5e47d3a91ae70e8560769d0
Rabi Prasad <rabiprasad607@gmail.com>
2021-10-02 01:36:00 +0530

    15-cocktail single cocktail page added

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
 
     try {
@@ -39,11 +39,11 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  };
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchDrinks();
-  }, [searchTerm]);
+  }, [searchTerm, fetchDrinks]);
   return (
     <AppContext.Provider
       value={{ loading, searchTerm, cocktails, setSearchTerm }}
