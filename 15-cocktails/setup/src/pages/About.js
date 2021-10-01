92afcbaa4c484288ff930a169093e7c292c925c8
Rabi Prasad <rabiprasad607@gmail.com>
2021-10-01 22:01:21 +0530

    15-cocktails basic file structure created

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure, aut?
+        Minus, architecto nostrum explicabo earum atque facilis adipisci, error
+        fugit assumenda cupiditate voluptates tempora animi nulla? Non nisi quas
+        expedita aperiam hic velit, sed maxime eveniet deleniti voluptatum.
+        Ducimus, quidem!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
