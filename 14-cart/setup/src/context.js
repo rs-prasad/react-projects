87250c2fb5bfc2267dbce38b844a20481f9117ac
Rabi Prasad <rabiprasad607@gmail.com>
2021-10-01 21:05:52 +0530

    14-cart fetch api functionality addded

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -31,7 +31,11 @@ const AppProvider = ({ children }) => {
 
   const fetchData = async () => {
     dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
   };
+  useEffect(() => fetchData(), []);
   useEffect(() => {
     dispatch({ type: "GET_TOTALS" });
   }, [state.cart]);
